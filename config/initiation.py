03b02d94c43dfc023270db3515f6da7249c177b4
XZNON <shivaliksingh09@gmail.com>
2025-08-11 00:34:40 +0530

    fixed duplicates

--- config/initiation.py
+++ config/initiation.py
@@ -1,6 +1,5 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
